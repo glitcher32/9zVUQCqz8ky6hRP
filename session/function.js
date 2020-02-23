00868babc581b4a19b91393b67eb58ab0ca8f18f
glitcher32 <26526046+glitcher32@users.noreply.github.com>
2020-02-23 07:24:06 +0000

    Update function.js

--- session/function.js
+++ session/function.js
@@ -1,14 +1,11 @@
 var toCopy  = document.getElementById( 'copy-text' ),
     btnCopy = document.getElementById( 'copy' ),
-    paste   = document.getElementById( 'empty-field' );
  
 btnCopy.addEventListener( 'click', function(){
   toCopy.select();
-  paste.value = '';
   
   if ( document.execCommand( 'copy' ) ) {
       btnCopy.classList.add( 'copied' );
-    paste.focus();
     
       var temp = setInterval( function(){
         btnCopy.classList.remove( 'copied' );
